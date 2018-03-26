5e61a02db5a5c58c247087425734c365e45cfd21
ljmgps <ljmgps@163.com>
2018-03-26 12:35:09 +0800

    mergepdf alldirs

--- merge_pdf.py
+++ merge_pdf.py
@@ -5,18 +5,16 @@ from PyPDF2 import PdfFileMerger, PdfFileReader
 
 import os
 
-def merge_pdf():
-    filepath = input("输入文件夹路径：")
+def merge_to_pdf(filepath):
     all_files = [os.path.join(filepath, p) for p in os.listdir(filepath)]
     pdf_files = list(filter(lambda f:os.path.splitext(f)[-1] == '.pdf', all_files))  # 筛选出pdf文件
 
-    merge_files = pdf_files  # 合并pdf还是合并所有
+    merge_files = pdf_files
     pp_merger = PdfFileMerger()
     merge_num = 0
     for i in range(len(merge_files)):
         try:
             with open(merge_files[i], 'rb') as f:
-                # f.write("%%EOF")
                 pp_file = PdfFileReader(f)
                 pp_merger.merge(merge_num, pp_file)
                 pages_num = pp_file.getNumPages()
@@ -27,5 +25,14 @@ def merge_pdf():
     with open(os.path.join(filepath, os.path.basename(filepath) + '.pdf'), 'wb') as f:
         pp_merger.write(f)
 
+def merge_all_dir(dir_path):
+    dir_list = [os.path.join(dir_path, x) for x in os.listdir(dir_path)]
+    dir_list = filter(lambda x: os.path.isdir(x), dir_list)
+    for d in dir_list:
+        merge_to_pdf(filepath=d)
+
+
 if __name__ == "__main__":
-    merge_pdf()
+    filepath = input("输入文件夹路径：")
+    merge_all_dir(filepath)
+    # merge_to_pdf(filepath)
