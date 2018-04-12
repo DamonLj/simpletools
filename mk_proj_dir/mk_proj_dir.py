7800602c3e67e2b98965785e0c999f2acfc446a8
DamonLj <ljdamon@163.com>
2018-04-12 22:30:41 +0800

    def mk dir from tree

--- mk_proj_dir/mk_proj_dir.py
+++ mk_proj_dir/mk_proj_dir.py
@@ -6,6 +6,9 @@ import os
 
 
 def txt_to_tree(path):
+    '''
+    把缩进结构关系转化为类似[(root1,dir1), (root2,dir2)]关系
+    '''
     tree = []
     with open(path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
@@ -14,18 +17,22 @@ def txt_to_tree(path):
         if not lines[i].strip() or lines[i].strip().startswith('#'):
             continue
         elif lines[i].startswith(' ') or lines[i].startswith('\t'):
-            pass
+            line = lines[i].rstrip()
+            
         else:
             pass
     return tree
 
 
-def mk_dir_from_tree(tree):
-    pass
+def mk_dir_from_tree(root, tree):
+    os.chdir(root)
+    for root, dir in tree:
+        os.mkdir(os.path.join(root, dir)
 
 
 if __name__ == "__main__":
     path = 'tree.txt'
     tree = txt_to_tree(path)
-    mk_dir_from_tree(tree)
+    root = input("输入创建文件夹的位置：")
+    mk_dir_from_tree(root, tree)
 
