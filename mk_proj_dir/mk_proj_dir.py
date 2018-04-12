714353c710cdbdd926ff589e3d38cde533af32ff
ljmgps <ljmgps@163.com>
2018-04-12 17:49:56 +0800

    gouzao

--- mk_proj_dir/mk_proj_dir.py
+++ mk_proj_dir/mk_proj_dir.py
@@ -5,6 +5,27 @@
 import os
 
 
-def to_dic():
-    tree = {}
+def txt_to_tree(path):
+    tree = []
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    for i in range(len(lines)):
+        if not lines[i].strip() or lines[i].strip().startswith('#'):
+            continue
+        elif lines[i].startswith(' ') or lines[i].startswith('\t'):
+            pass
+        else:
+            pass
+    return tree
+
+
+def mk_dir_from_tree(tree):
+    pass
+
+
+if __name__ == "__main__":
+    path = 'tree.txt'
+    tree = txt_to_tree(path)
+    mk_dir_from_tree(tree)
 
